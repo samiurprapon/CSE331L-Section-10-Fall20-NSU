343012d0d7e8c6106917241377e6c5dedb5aa2e9
Samiur Prapon <samiur.prapon@gmail.com>
2020-11-11 10:29:22 +0600

    Lab assessment 1 complete

--- Lab Assessment Submission/Lab 1/lab1.asm
+++ Lab Assessment Submission/Lab 1/lab1.asm
@@ -2,15 +2,17 @@
 ; You may customize this and other start-up templates; 
 ; The location of this template is c:\emu8086\inc\0_com_template.txt
 
-org 100h
+org 100h 
+                  
+MOV AX, 1
+MOV BX, 5
+MOV CX, 5
 
-MOV AX, 14; add your code here
-MOV BX, 15
 
-ADD AX, BX
+ADD BX, CX
+ADD AX ,BX
 
 ret
-
 
 
 
